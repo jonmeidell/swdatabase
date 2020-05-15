855c655e3512a197ba03892c3ba3f1e84ada47a3
jonmeidell <jonmeidell@gmail.com>
2020-05-14 21:15:57 -0600

    minor fixes

--- server.js
+++ server.js
@@ -130,6 +130,7 @@ app.post("/api/charactersE", function (req, res) {
 
 // Starts the server to begin listening
 // =============================================================
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, function () {
 	console.log("App listening on PORT " + PORT);
 });
